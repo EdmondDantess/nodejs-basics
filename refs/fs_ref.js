81ffcddb4ebc01cf82c9e6256d2b38625c3f6c4f
Maksim Laurouski <maximlavrovsky@mail.ru>
2023-02-07 18:12:19 +0300

    feat: os fs modules

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -1,3 +1,45 @@
-const fs = require('fs')
+const fs = require('node:fs')
+const path = require('path')
+
 
 //File system
+// fs.mkdir(path.join(__dirname, 'notes'), (e) => {
+//     if (e) throw new Error(e)
+//
+//     console.log('Папка создана')
+// })
+
+// fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'),
+//     'Hello world',
+//     (e) => {
+//         if (e) throw new Error(e)
+//         console.log('Файл был создан')
+//
+//         fs.appendFile(
+//             path.join(__dirname, 'notes', 'mynotes.txt'),
+//             ' From append file',
+//             e => {
+//                 if (e) throw new Error(e)
+//                 console.log('Файл был изменён')
+//             }
+//         )
+//
+//         fs.readFile(
+//             path.join(__dirname, 'notes', 'mynotes.txt'),
+//             'utf-8',
+//             (e, data) => {
+//                 if (e) throw new Error(e)
+//                 console.log(data)
+//             }
+//         )
+//     }
+// )
+
+fs.rename(
+    path.join(__dirname, 'notes', 'mynotes.txt'),
+    path.join(__dirname, 'notes', 'note.txt'),
+    (err) => {
+        if (err) throw err
+        console.log('Rename complete!')
+    }
+)
---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+//Platform
+console.log(os.platform())
+
+//Aрхитектура
+console.log(os.arch())
+
+// Инфо
+console.log(os.cpus())
+
+// Свободная память
+console.log(os.freemem())
+
+//Cколько памяти всего
+console.log(os.totalmem())
+
+// Корневая директория
+console.log(os.homedir())
+
+//Сколько система работает
+console.log(os.uptime())
